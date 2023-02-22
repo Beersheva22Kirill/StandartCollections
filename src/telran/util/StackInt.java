92fc746f6fa5989ce572367f939e67c2e2e9411b
kirill2884 <kirill2884@gmail.com>
2023-02-22 23:44:09 +0200

    HW 33

--- src/telran/util/StackInt.java
+++ src/telran/util/StackInt.java
@@ -1,23 +1,20 @@
 package telran.util;
 
-import java.util.Comparator;
+
 import java.util.LinkedList;
-import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.OptionalInt;
+
 
 public class StackInt {
+	private LinkedList<Integer> list;
 	
-	List<Integer> list = new LinkedList<>();
-	Integer currentMax;
-	
+	public StackInt() {
+		list = new LinkedList<>();
+
+	}
+
 	public void push(int element) {
-		if (currentMax == null) {
-			currentMax = element;
-		}else if (element > currentMax) {
-			currentMax = element;
-		}
+		
 		list.add(element);
 	}
 	
@@ -25,8 +22,8 @@ public class StackInt {
 		if (isEmpty()) {
 			throw new NoSuchElementException();
 		}
-		
-		return list.get(list.size() - 1);
+
+		return list.pollLast();
 	}
 	
 	public boolean isEmpty () {
@@ -38,6 +35,9 @@ public class StackInt {
 		if (isEmpty()) {
 			throw new NoSuchElementException();
 		}
-		return currentMax;
+		
+		return list.stream().mapToInt(num -> num).max().getAsInt();
 	}
+
+
 }
