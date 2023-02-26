8c169e9f08514e7c3f8becd1aedf1609a0787d7b
kirill2884 <kirill2884@gmail.com>
2023-02-26 21:05:56 +0200

    HW 34 Update (LogN)

--- src/telran/structure/ItemCounter.java
+++ src/telran/structure/ItemCounter.java
@@ -1,31 +1,50 @@
 package telran.structure;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.OptionalInt;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 
 public class ItemCounter implements MultiCounters {
 	
 	private Map<Object, Integer> items;
+	private Map.Entry<Object, Integer> entry;
+	private TreeMap <Integer, HashSet<Object>> itemsForMax;
 	
 	public ItemCounter() {
-		items = new HashMap<Object, Integer>();
+		items = new HashMap<Object, Integer>();			
+		itemsForMax = new TreeMap<>(); 	  
 	}
 	
 	@Override
 	public Integer addItem(Object item) {
+	
 		
 		Integer entry = items.get(item);
 		if (entry != null) {
 			items.put(item, ++entry);
-		} else {
-			items.put(item, 1);
-			entry = 1;
-		}	
+			
+			} else {
+				items.put(item, 1);
+				entry = 1;
+			}	
+		
+		HashSet<Object> set = itemsForMax.get(entry);
+		if (set != null) {
+			set.add(item);
+			} else {
+				set = new HashSet<>();
+				set.add(item);
+				itemsForMax.put(entry, set);
+			}
+			
 		return entry;
 	}
 
@@ -38,8 +57,15 @@ public class ItemCounter implements MultiCounters {
 	@Override
 	public boolean remove(Object item) {
 		Integer entry = items.get(item);
+		
+		
 		boolean res = false;
 		if (entry != null) {
+			HashSet<Object> set = itemsForMax.get(entry);
+			set.remove(item);
+			if (set.isEmpty()) {
+				itemsForMax.remove(entry);
+			}
 			items.remove(item);
 			res = true;
 		}
@@ -48,15 +74,8 @@ public class ItemCounter implements MultiCounters {
 
 	@Override
 	public Set<Object> getMaxItems() {
-		Set<Object> res = new HashSet<>();
-		Set<Entry<Object, Integer>> setEntry = items.entrySet();
-		
-			Collection<Integer> itemsValues = items.values();
-			OptionalInt max = itemsValues.stream().mapToInt(n -> n).max();		
-						
-		setEntry.stream().filter(n -> n.getValue().equals(max.getAsInt())).forEach(n -> res.add(n.getKey()));
-		
-		return res;
+	
+		return itemsForMax.get(itemsForMax.lastKey());
 	}
 
 
